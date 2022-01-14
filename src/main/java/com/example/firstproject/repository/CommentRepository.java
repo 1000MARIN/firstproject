f8aefab222e8bb8505bd910783b1d1248e06230c
1000marin <leexjin@gmail.com>
2022-01-14 17:09:42 +0900

    Update CommentRepository.java

--- src/main/java/com/example/firstproject/repository/CommentRepository.java
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -3,20 +3,20 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> { //<관리할 대상, 댕상의 FK(외래키)
-    // 특정 게시글의 모든 댓글 조회
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 어노테이션 기반 SQL 조회: 특정 게시글(article)의 모든 댓글(comment)
     @Query(value =
             "SELECT * " +
             "FROM comment " +
             "WHERE article_id = :articleId",
             nativeQuery = true)
-    List <Comment> findByArticleId(Long articleId);
-
-    // 특정 닉네임의 모든 댓글 조회
-    List<Comment> findByNickname(String nickname);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
+    // XML 기반 SQL 조회: 특정 닉네임(nickname)의 모든 댓글(comment)
+    List<Comment> findByNickname(@Param("nickname") String nickname);
 
 
 }
