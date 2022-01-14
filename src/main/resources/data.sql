9713c68f415241333afa7896028d2d30b91b13bb
1000marin <leexjin@gmail.com>
2022-01-14 17:09:48 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(id, title, content) VALUES ( 1, '가가가가', '1111' );
-INSERT INTO article(id, title, content) VALUES ( 2, '나나나나', '2222' );
-INSERT INTO article(id, title, content) VALUES ( 3, '다다다다', '3333' );
+INSERT INTO article(id, title, content) VALUES (1, '가가가가', '1111');
+INSERT INTO article(id, title, content) VALUES (2, '나나나나', '2222');
+INSERT INTO article(id, title, content) VALUES (3, '다다다다', '3333');
 
 -- article 더미 데이터 추가
 INSERT INTO article(id, title, content) VALUES (4, '당신의 인생 영화는?', '댓글 ㄱ');
